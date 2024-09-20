9615b8aa87dd54563dae76e5f9a4ab2e38fad7d2
BloopSmasher <149552751+BloopSmasher@users.noreply.github.com>
2024-09-20 18:55:01 +0530

    Adding Source Files

---
+++ All in One Runtime Installer (AutoAdmin).bat
@@ -0,0 +1,80 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Check for admin rights
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+:: Ask the user if they want to continue running the script
+echo Do you want to run the script? [Y/N]
+choice /c YN /n /m "Press Y to continue or N to cancel: "
+if errorlevel 2 goto end
+if errorlevel 1 goto continue
+
+:continue
+@echo off
+SETLOCAL ENABLEEXTENSIONS
+
+echo Installing all the required runtime packages in your system....
+echo.
+
+REM KILL ANY RUNNING INSTANCES OF DISM OR TIWORKER TO AVOID ERRORS
+TASKLIST | FINDSTR "Dism.exe TiWorker.exe" >NUL && TASKKILL /F /IM "Dism.exe" /IM "TiWorker.exe" /T >NUL 2>&1
+
+@echo off
+CD /d %~dp0
+
+CALL "Visual-C-Runtimes\Install All Visual C++ Redistributable Runtimes.bat"
+CALL "DirectX-Redist-Jun-2010\DirectX Runtime Offline Installer.bat"
+CALL ".Net-Installers\.Net Installer.bat"
+
+ECHO Successfully installed all Microsoft's Visual C Runtimes, DotNet SDK LTS Runtimes, and DirectX!
+ECHO.
+
+ENDLOCAL
+
+@echo off
+setlocal enabledelayedexpansion
+
+for /l %%i in (10,-1,1) do (
+    cls
+	echo A Reboot will be required to finish the Installation..
+    echo Your computer will be restarted in %%i seconds.
+    echo Press "N" to cancel or "R" to reboot immediately.
+    choice /c NRT /t 1 /d T > nul
+    if !errorlevel! equ 1 (
+        cls
+		ENDLOCAL
+        echo Restart Canceled. You can reboot later.
+        echo.
+        echo Press any key to exit...
+        pause > nul
+        exit /b
+    ) else if !errorlevel! equ 2 (
+        goto :reboot
+    )
+)
+
+:reboot
+cls
+ENDLOCAL
+echo Restarting now...
+shutdown /r /t 0
+
+
+
+
